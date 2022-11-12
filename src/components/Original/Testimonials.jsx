fd8609b3ff9ce2b009f04e5572d5f22bd30ad38e
Oleg Yadroshnikov <contact@olegyadroshnikov.com>
2022-11-12 02:12:12 -0600

    Add files via upload

--- src/components/Original/Testimonials.jsx
+++ src/components/Original/Testimonials.jsx
@@ -23,43 +23,43 @@ const Testimonials = () => {
           <p className='pt-4 text-md'>This is how I am described by others</p>
         </div>
 
-        <div className='grid w-full grid-cols-1 gap-4 py-8 ml-16 text-center lg:grid-cols-2 xl:grid-cols-3 lg:ml-0'>
-          <div className='shadow-md w-[155px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 flex justify-center items-center hover:shadow-[#3764c5]'>
+        <div className='grid w-full grid-cols-1 gap-2 py-4 ml-20 text-center sm:py-8 sm:gap-4 lg:grid-cols-2 xl:grid-cols-3 lg:ml-0'>
+          <div className='shadow-md w-[180px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 flex justify-center items-center hover:shadow-[#3764c5]'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial1}
               alt='Tesimonial1 screenshot'
             />
           </div>
-          <div className='shadow-md w-[155px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
+          <div className='shadow-md w-[180px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial2}
               alt='Tesimonial2 screenshot'
             />
           </div>
-          <div className='shadow-md w-[155px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
+          <div className='shadow-md w-[180px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial3}
               alt='Tesimonial3 screenshot'
             />
           </div>
-          <div className='shadow-md w-[155px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
+          <div className='shadow-md w-[180px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 hover:shadow-[#3764c5]'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial4}
               alt='Tesimonial4 screenshot'
             />
           </div>
-          <div className='shadow-md w-[155px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 flex justify-center items-center hover:shadow-[#3764c5]'>
+          <div className='shadow-md w-[180px]  md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:scale-150 duration-700 flex justify-center items-center hover:shadow-[#3764c5]'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial5}
               alt='Tesimonial5 screenshot'
             />
           </div>
-          <div className='shadow-md w-[155px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:shadow-[#3764c5] hover:scale-150 duration-700 flex justify-center items-center'>
+          <div className='shadow-md w-[180px] md:w-[320px] lg:w-[380px] rounded-xl shadow-[#040c16] hover:shadow-[#3764c5] hover:scale-150 duration-700 flex justify-center items-center'>
             <img
               className='w-full mx-auto rounded-xl'
               src={Testimonial6}
